d22de3d2a99a30b6fea8f39cd93fb0b03220ebc8
fumi uk <dcfumiya.238@gmail.com>
2021-03-31 07:24:10 +0900

    basic-ts

--- src/Article.tsx
+++ src/Article.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Title } from "./Blog";
-import LikeButton from "./LikeButton";
+// import LikeButton from "./LikeButton";
 
 const Article = (props: Title) => {
+  const [isPublished, togglePublished] = useState(false);
   return (
     <div>
       <h2>{props.title}</h2>
       <p>順番は{props.order}です。</p>
       <p>著者：{props.author}</p>
       <label htmlFor="check">公開状態：</label>
-      <input type="checkbox" checked={props.isPublished} onClick={() => props.toggle()}></input>
-      <LikeButton count={props.count} />
+      <input type="checkbox" checked={isPublished} onClick={() => togglePublished(!isPublished)}></input>
+      {/* <LikeButton count={props.count} /> */}
     </div>
   );
 }
--- src/Blog.tsx
+++ src/Blog.tsx
@@ -6,10 +6,8 @@ import Hoge from "./components/Hoge";
 export type Title = {
   title: string,
   order: number,
-  isPublished: boolean,
-  toggle: () => void,
   author: string,
-  count: number
+  // count: number
 };
 
 export type MyState = {
@@ -17,59 +15,44 @@ export type MyState = {
   count: number
 };
 
-export default class Blog extends React.Component<{}, MyState> {
-  constructor(props: Title) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount() {
-    document.getElementById("counter")?.addEventListener("click", () => this.countUp());
-  }
+  // componentDidMount() {
+  //   document.getElementById("counter")?.addEventListener("click", () => this.countUp());
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState(
-        { count: 0 }
-      );
-    }
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState(
+  //       { count: 0 }
+  //     );
+  //   }
+  // }
 
-  componentWillUnmount() {
-    document.getElementById("counter")?.removeEventListener("counter", () => this.countUp());
-  }
+  // componentWillUnmount() {
+  //   document.getElementById("counter")?.removeEventListener("counter", () => this.countUp());
+  // }
 
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    });
-  };
+  // countUp = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   });
+  // };
 
-  countUp = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-
-  render() {
-    const authorName: string = "user";
-    return (
-      <div>
-        <Article
-          title={"React の使い方"}
-          order={1}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          author={authorName}
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </div>
-    );
-  }
+  const authorName: string = "user";
+  return (
+    <div>
+      <Article
+        title={"React の使い方"}
+        order={1}
+        author={authorName}
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </div>
+  );
 }
+
+export default Blog;
